020f51114a5fd1c797f136ed11695380a7046ac7
linacd <linacd@hawaii.edu>
2023-09-03 08:43:14 -1000

    Assign #2 Completed Swift Playgrounds#1
    Introduction to Variables & Strings

--- IntrotoSwiftPlaygrounds_1.playground/Contents.swift
+++ IntrotoSwiftPlaygrounds_1.playground/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 var greeting = "Hello, playground"
-
+//Assignment #2 Intro to Variables & String
 //var myFirstVariable = "Iʻve created my first variable"
 //var is only used when declaring the variable
 var myfirstvariable: String
@@ -9,7 +9,7 @@ var myfirstvariable: String
 var myFirstVariable = "Iʻve created my first variable"
 //myFirstVariable = "5"
 
-/* use this to comment a block of code8*/
+/* use this to comment a block of code*/
 // this is how to comment out a single line of code
 
 var mySecondVariable = "and look I added 2 strings together"
@@ -20,6 +20,30 @@ var mySecondVariable = "and look I added 2 strings together"
 var myThirdVariable = myFirstVariable + mySecondVariable
 
 print(myThirdVariable)
+
+//COMPLETED part 1-8 from Assignment #2
+//Assignment #2 Part 9 on your own
+
+var myFourthVariable: String
+
+myFourthVariable = "Ive created my fourth variable"
+
+var myFifthVariable: String
+
+myFifthVariable = "Ive created my fifth variable"
+
+var mySixthVariable: String
+
+mySixthVariable = "Ive created my sixth variable"
+
+var mySeventhVariable = (myFourthVariable) + " " + (myFifthVariable) + " " + (mySixthVariable)
+
+print(mySeventhVariable)
+
+//Part 9 I have created 4 variables with a concetrated string plus added spaces between vars
+//Part 10 COMPLETED
+//Assignment #2 COMPLETED
+
 
 //Assignment #3 Introduction to True andx False
 
@@ -37,6 +61,25 @@ var numberTwo = 7
 
 var comparison = numberOne > numberTwo
 
-//Part 7 On your own: completed
+//Completed part 1-6
 
-//Part 8 On your own Part 2: completed
+//Part 7 On your own:
+// I will create two new variables and assign those variable to two different strings
+
+var newvar1: String
+
+newvar1 = "Ive created a new var"
+
+var newvar2: String
+
+newvar2 = "Ive created a second var"
+
+
+//I will create a third variable that is to the statement that evaluates my newvar1 and newvar2 to equal to each other
+
+var newvar3 = newvar1 == newvar2
+
+print(newvar3)
+
+
+//Part 8 On your own: completed
